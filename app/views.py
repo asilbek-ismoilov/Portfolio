9ac573c6955e5ac54bd5e5a591cc34c7261b9727
asilbek-ismoilov <asilbe.m00d@gmail.com>
2024-09-13 03:48:14 +0300

    contact

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Experience, Education, WorkCategory, Work, Blog, Contact
 from django.views.generic.detail import DetailView
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index_view(request):
     experiences = Experience.objects.all()
@@ -9,6 +12,19 @@ def index_view(request):
     workcategs = WorkCategory.objects.all()
     educations = Education.objects.all()
     blogs = Blog.objects.all()
+
+    if request.method == 'POST':
+        try:
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            message = request.POST.get('message')
+            new_contact = Contact(name=name, email=email, message=message)
+            print(new_contact)
+            new_contact.save()
+            messages.success(request, "Sizning xabaringiz yuborildi!!!")
+            return HttpResponseRedirect(reverse('home-page'))
+        except Exception as e:
+            messages.error(request, "Xabaringiz yuborishda xatolik yuz berdi. Iltimos, qayta urinib ko'ring.")
 
     context = {
         "experiences": experiences,
@@ -25,5 +41,3 @@ class BlogDetailView(DetailView):
     context_object_name = 'blog'
     template_name = 'blog.html'
     slug_field = 'slug'
-
-
